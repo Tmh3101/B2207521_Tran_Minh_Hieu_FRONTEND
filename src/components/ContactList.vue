283a9d8c5c5bfb0eaa52cd92bc451e42555c3c5d
Minh Hiểu Trần <minhhieu31012004@gmail.com>
2025-05-14 16:16:08 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/ContactList.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  props: {
+    constacts: { type: Array, default: [] },
+    activeIndex: { type: Number, default: -1 },
+  },
+  emits: ["update:activeIndex"],
+  methods: {
+    updateActiveIndex(index) {
+      this.$emit("update:activeIndex", index);
+    },
+  },
+};
+</script>
+
+<template>
+  <ul class="list-group">
+    <li
+      class="list-group-items"
+      v-for="(constact, index) in constacts"
+      :key="constact._id"
+      :class="{ active: index === activeIndex }"
+      @click="updateActiveIndex(index)"
+    >
+      {{ constact.name }}
+    </li>
+  </ul>
+</template>
